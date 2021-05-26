2e754c998666237aef7199843023146fc7ede868
Mikael Arneborn <marneborn@gmail.com>
2021-05-26 03:25:44 -0700

    upload asset on release

--- .github/actions/create-release/index.js
+++ .github/actions/create-release/index.js
@@ -1,35 +1,67 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const semver = require('semver');
+const path = require('path');
+const fs = require('fs');
 
 const owner = 'marneborn';
 const repo = 'okh-groups-map';
 
-const version = core.getInput('version');
-const prNum = core.getInput('pr-num');
-const token = core.getInput('GITHUB_TOKEN');
-const octokit = github.getOctokit(token)
+const version = '0.1.8';
+const prNumber = '9';
+console.log(JSON.stringify(process.env, null, 2));
+const token = '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';
+console.log('token', token);
+const octokit = github.getOctokit(token);
 
 async function run() {
   try {
-    const release = await octokit.rest.repos.getLatestRelease({ owner, repo });
-
     const createReleaseResponse = await octokit.rest.repos.createRelease({
       owner,
       repo,
       tag_name: version,
-      name: `v${version} - new Date().toISOString()`,
-      body: `https://github.com/${owner}/${repo}/pull/${prNum}`,
+      name: `v${version} - ${new Date().toISOString()}`,
+      body: `https://github.com/${owner}/${repo}/pull/${prNumber}`,
       draft: false,
       prerelease: false,
-      target_commitish: 'main'
+      target_commitish: 'main',
     });
 
-    console.log('new release: ', console.log(createReleaseResponse, null, 2));
-  }
-  catch (error) {
+    const release = createReleaseResponse.data;
+
+    const {
+      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl },
+    } = createReleaseResponse;
+    core.debug(`Create release ${version} (${releaseId}): ${htmlUrl}.`);
+
+    const assetName = 'index.js';
+    const file = path.resolve('dist/index.js');
+    const stat = fs.statSync(file);
+    if (!stat.isFile()) {
+      core.setFailed('Could not upload dist/index.js');
+      return;
+    }
+
+    const fileSize = stat.size;
+    const fileBytes = fs.readFileSync(file);
+
+    core.debug(`Uploading ${file} to ${assetName} in release ${version}.`);
+    const uploadResponse = await octokit.rest.repos.uploadReleaseAsset(
+      {
+        url: uploadUrl,
+        name: assetName,
+        data: fileBytes,
+        headers: {
+          'content-type': 'binary/octet-stream',
+          'content-length': fileSize,
+        },
+      },
+    );
+    core.debug(`Added ${assetName}: ${uploadResponse.data.browser_download_url}`);
+    return uploadResponse.data.browser_download_url;
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
