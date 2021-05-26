cf8fec512c2c8c63268410b5d4c46efbfb52a010
Mikael Arneborn <marneborn@gmail.com>
2021-05-26 00:10:46 -0700

    check releaseable

--- .github/actions/check-releaseable/index.js
+++ .github/actions/check-releaseable/index.js
@@ -1,10 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const labelToVersion = {
+  'release-major': 'major',
+  'release-minor': 'minor',
+  'release-patch': 'patch',
+};
+
 try {
-  const labelNames = github.context.pull_request.labels.map(label => label.name)
+  const labelNames = github.context.pull_request.labels
+    .map(label => label.name)
+    .filter(labelName => labelToVersion[labelName]);
+
   console.log(JSON.stringify(labelNames))
-  throw new Error('oops')
+
+  if (labelNames.length === 0) {
+    core.setFailed('No labels set');
+  } else if (labelNames.length > 1) {
+    core.setFailed(`Multiple labels set: ${labelNames.join(', ')}`);
+  } else {
+    console.log('dsfdsfds', labelToVersion[labelNames[0]]);
+    core.setOutput('versionType', labelToVersion[labelNames[0]]);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
